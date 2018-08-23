7d73b5c13d4d26b981618c27bfb596291778a162
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-23 12:56:54 -0400

    made it work

---
+++ TAClassdrill/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAClassdrill
+{
+    class Numbers
+    {
+        public void Addition(int num1, int num2)
+        {
+            int result = num1 + 20;
+            int result2 = num2;
+            Console.WriteLine("The numbers added equal : {0}", result);
+            Console.WriteLine("The second number is : {0}", result2);
+            return;            
+        }
+    }
+}
--- TAClassdrill/Program.cs
+++ TAClassdrill/Program.cs
@@ -10,6 +10,21 @@ namespace TAClassdrill
     {
         static void Main(string[] args)
         {
+            Numbers math = new Numbers();
+            math.Addition(10, 20);
+            math.Addition(num1:10, num2:20);
+
+            Console.ReadLine();
         }
     }
 }
+
+//1. Create a class. In that class, create a void method that takes
+//   two integers as parameters.Have the method do a math operation 
+//   on the first integer and display the second integer to the screen.
+
+//2. In the Main() method of the console app, instantiate the class.
+
+//3. Call the method in the class, passing in two numbers.
+
+//4. Call the method in the class, specifying the parameters by name.
